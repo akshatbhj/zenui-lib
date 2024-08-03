c0763d324664f6c2fceb9f1c24c1de8b9b6e214e
Akshat Bhardwaj <info.bhardwajakshat@gmail.com>
2024-08-03 20:33:59 +0530

    feat: added storybook and tailwind css

--- lib/Button/Button.tsx
+++ lib/Button/Button.tsx
@@ -4,5 +4,9 @@ interface ButtonProps {
   children: ReactNode;
 }
 export const Button = ({ children }: ButtonProps): ReactNode => {
-  return <button>{children}</button>;
+  return (
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-8 rounded">
+      {children}
+    </button>
+  );
 };
